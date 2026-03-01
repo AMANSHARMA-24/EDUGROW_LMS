f2150c83e025a8fc07e414568f9a358ff637cc08
AMANSHARMA <145786279+AMANSHARMA-24@users.noreply.github.com>
2026-03-01 18:55:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-   origin:"http://localhost:5173",
+   origin:"https://edugrow-lms-frontend.onrender.com",
    credentials: true,
 }))
 
